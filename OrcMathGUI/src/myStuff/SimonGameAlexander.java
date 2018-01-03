c9b8143d5ff6a56731cfb8430259d261497370fa
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-01-03 10:42:11 -0500

    patient comment

---
+++ OrcMathGUI/src/myStuff/SimonGameAlexander.java
@@ -0,0 +1,14 @@
+package myStuff;
+
+public class SimonGameAlexander {
+
+	public SimonGameAlexander() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
